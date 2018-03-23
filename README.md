eb4df05fb9e0ad21b1c3c5f04c736470b6c19a97
john <john.stevens-garmon@cfpb.gov>
2018-03-23 15:48:49 -0400

    updates

---
+++ README.md
@@ -0,0 +1,43 @@
+# R Package Builder: for storing specific versions of R packages
+
+## Ad-hoc method of adding packages
+#### Adding a package to the repo
+To add a new package to this repo (e.g., `tweenr`, version `0.1.5`):
+
+1. Add it's zip file (e.g., `tweenr_0.1.5.zip`) to `bin/windows/contrib/3.4`
+2. From the local directory where the zip files are (`setwd(bin/windows/contrib/3.4)`), run this is R to generate the PACKAGES index file:
+	```
+	require(tools)
+	tools::write_PACKAGES('.', type='win.binary')
+	```
+3. Upload these files to `bin/windows/contrib/3.4`:
+	```
+	git add .
+	git commit -m "adding new packages"
+	git push origin master
+	```
+
+#### Installing a package from this repo
+The package can then be installed using the `repos=https://oblatespheroid.github.io/R_pkg_builder` argument. E.g.:
+
+    install.packages('tweenr', repos='repos=https://oblatespheroid.github.io/R_pkg_builder', type='win.binary')
+
+
+## Scripted method of adding multiple packages
+
+#### Requirements:
+ - Python 3
+ - packages in `requirements.txt`.
+   - Within a virtual env, run: `pip install requirements-pip.txt`
+   - Or, to create a conda environment with needed packages, run:
+     ```
+     conda env create -n Rpkgr -f requirements-conda.yml
+     activate Rpkgr
+     ```
+
+#### Run:
+In Windows, from the main directory in your local repo (`RPackages`), something like:
+
+    python R_pkg_builder.py LIBFOLDER=%RHOME%/library OUTFOLDER=%cd%/bin/windows/contrib/3.4
+
+If your packages are in another folder, change LIBFOLDER to that directory.
--- R_pkg_builder.py
+++ R_pkg_builder.py
@@ -1,9 +1,9 @@
 '''This script points to an R library directory (LIBFOLDER) where built R
-packages are (non-zipped) and--provided a zip executable (ZEXEC)--creates
-an R package repositiory in the output directory (OUTFOLDER).
+packages are (non-zipped) and creates an R package repositiory in the
+output directory (OUTFOLDER).
 
-LIBFOLDER, OUTFOLDER, and ZEXEC can be passed as arguments:
-python R_pkg_builder.py LIBFOLDER=/usr/lib/R/library OUTFOLDER=$(pwd) ZEXEC=$(which zip)
+LIBFOLDER and OUTFOLDER can be passed as arguments:
+python R_pkg_builder.py LIBFOLDER=/usr/lib/R/library OUTFOLDER=$(pwd)
 
 The zip files and the PACKAGES file this script creates in the output
 directory can be used in R's install.packages() function, as in:
@@ -11,65 +11,78 @@ install.packages(repos='path to repo', type='win.bin')
 '''
 
 import os, sys
-from subprocess import call, check_output
+import zipfile
 from rpy2.robjects.packages import importr
 
-# Pull variables in from passed arguments:
-if len(sys.argv) > 1:
-    for arg in sys.argv[1:len(sys.argv)]:
+
+class RPkgr():
+    def __init__(self, LIBFOLDER=None, OUTFOLDER=None):
+        '''Pulls variables in from passed arguments. Also allows
+           user to pass when creating instance.'''
+        if len(sys.argv) > 1:
+            for arg in sys.argv[1:len(sys.argv)]:
+                try:
+                    k,v = arg.split('=')
+                    v = v.replace('\\','/') # change windows to unix style
+                    exec('self.'+k.upper()+'='+"\'"+v+"\'")
+                except:
+                    pass
         try:
-            k,v = arg.split('=')
-            exec(k.upper()+'='+"\'"+v+"\'")
-        except:
-            pass
-
-
-def extract_version(file):
-    '''Return version number from an R Description file.
-       Example:
-       extract_version('/usr/lib/R/library/base/DESCRIPTION')'''
-    stream = open(file, 'r').read().replace('\t','').split('\n')
-    d = {}
-    for x in stream:
+            assert(type(self.LIBFOLDER) == str)
+        except Exception as e:
+            raise TypeError("LIBFOLDER not specified, or not a string: {}".format(e))
         try:
-            k,v = x.split(':')
-            d[k] = v
-        except: pass
-    version = d['Version'].strip()
-    return version 
+            assert(type(self.OUTFOLDER) == str)
+        except Exception as e:
+            raise TypeError("OUTFOLDER not specified, or not a string: {}".format(e))
 
-def zipit(libfolder, outfolder, exec):
-    '''for each package in libfolder, extracts version number
-       to use in naming a zip file. Zips package to outfolder
-       in correct format.'''
-    old_wd = os.getcwd()
-    os.chdir(libfolder)
-    for dir in os.listdir():
+    def extract_version(self, file):
+        '''Return version number from an R Description file.
+           Example:
+           extract_version('/usr/lib/R/library/base/DESCRIPTION')'''
+        stream = open(file, 'r').read().replace('\t','').split('\n')
+        d = {}
+        for x in stream:
+            try:
+                k,v = x.split(':')
+                d[k] = v
+            except: pass # not all lines can be parsed, which is fine
+        version = d['Version'].strip() # we just need the Version line
+        return version
+
+    def zipit(self):
+        '''for each package in LIBFOLDER, extracts version number
+           to use in naming a zip file. Zips package to OUTFOLDER
+           in correct format.'''
+        comp_lev = zipfile.ZIP_DEFLATED
+        for dir in os.listdir(self.LIBFOLDER):
+            try:
+                libpath = os.path.join(self.LIBFOLDER, dir, 'DESCRIPTION')
+                v = self.extract_version(libpath)
+                zfile_name = self.OUTFOLDER+'/'+dir+'_'+v+'.zip'
+                if not os.path.isfile(zfile_name): # don't overwrite if exists
+                    zipf = zipfile.ZipFile(zfile_name, 'w', comp_lev)
+                    for root, dirs, files in os.walk(dir):
+                        for file in files:
+                            zipf.write(os.path.join(root, file))
+                    zipf.close()
+                    print('Successfully zipped {}'.format(dir))
+            except Exception as e:
+                print('Cannot zip {0}: {1}'.format(dir, e))
+
+    def create_PACKAGES(self):
+        '''Call R tools::write_PACKAGES function to create
+           PACKAGES file needed for repo'''
         try:
-            v = extract_version(dir+'/DESCRIPTION')
-            call([exec, '-r', outfolder+'/'+dir+'_'+v+'.zip',
-                  dir])
-        except:
-            pass
-    os.chdir(old_wd)
+            self.zipit()
+            rtools = importr('tools')
+            rtools.write_PACKAGES(self.OUTFOLDER, type='win.bin')
+            return 0
+        except Exception as e:
+            print(e)
+            return 1
 
-def create_PACKAGES(outfolder):
-    '''Call R tools::write_PACKAGES function to create
-       PACKAGES file needed for repo'''
-    rtools = importr('tools')
-    rtools.write_PACKAGES(outfolder, type='win.bin')
-
-def main(libfolder=LIBFOLDER, outfolder=OUTFOLDER, exec=ZEXEC):
-    '''Loops through directories in libfolder, zips them, names
-       zip files based on version number, and builds the PACKAGES file'''
-    zipit(libfolder, outfolder, exec)
-    create_PACKAGES(outfolder)
     
 if __name__ == '__main__':
-    if LIBFOLDER not in globals():
-        sys.exit('LIBFOLDER not set. Need to set LIBFOLDER, OUTFOLDER, and ZEXEC')
-    if OUTFOLDER not in globals():
-        sys.exit('OUTFOLDER not set. Need to set LIBFOLDER, OUTFOLDER, and ZEXEC')
-    if ZEXEC not in globals():
-        sys.exit('ZEXEC not set. Need to set LIBFOLDER, OUTFOLDER, and ZEXEC')
-    sys.exit(main())
+    pkgr = RPkgr()
+    sys.exit(pkgr.create_PACKAGES())
